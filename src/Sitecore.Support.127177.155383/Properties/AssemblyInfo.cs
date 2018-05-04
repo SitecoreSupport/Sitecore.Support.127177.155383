ef901b5c4aa92b7a117be7ce3f39d645d59a9a4f
DK\psk <psk@sitecore.net>
2018-05-04 14:15:05 +0300

    Update solution, project, assembly name

--- src/Sitecore.Support.127177.155383/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.127177.155383/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Sitecore.Support.127177.155383")]
-[assembly: AssemblyProduct("Sitecore.Support.127177.155383")]
+[assembly: AssemblyTitle("Sitecore.Support.96740.127177.155383")]
+[assembly: AssemblyProduct("Sitecore.Support.96740.127177.155383")]
 [assembly: ComVisible(false)]
